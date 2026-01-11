6fcaba2949b96b8f39641301755e23d41dfcbb76
yesung1215 <xocds991215@gmail.com>
2026-01-11 14:29:31 +0900

    [init] 초기 셋팅

---
+++ src/main/java/com/app/onmeet/OnMeetApplication.java
@@ -0,0 +1,13 @@
+package com.app.onmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnMeetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnMeetApplication.class, args);
+    }
+
+}
