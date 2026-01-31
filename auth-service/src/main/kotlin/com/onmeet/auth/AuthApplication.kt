527ef7acabc061b3b067cd32a9bffac88d7dc446
sprtms16 <sprtms16@gmail.com>
2026-01-31 15:14:54 +0900

    refactor: remove unused EnableJpaAuditing import in AuthApplication

--- auth-service/src/main/kotlin/com/onmeet/auth/AuthApplication.kt
+++ auth-service/src/main/kotlin/com/onmeet/auth/AuthApplication.kt
@@ -3,7 +3,7 @@ package com.onmeet.auth
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
 
 @SpringBootApplication
 class AuthApplication
