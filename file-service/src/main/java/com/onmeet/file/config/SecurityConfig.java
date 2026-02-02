d255b41ef6db74e38374a709d36ce3aa03296121
sprtms16 <sprtms16@gmail.com>
2026-02-02 16:30:09 +0900

    refactor: Replace image-service with file-service and update build settings

--- file-service/src/main/java/com/onmeet/file/config/SecurityConfig.java
+++ file-service/src/main/java/com/onmeet/file/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.onmeet.image.config;
+package com.onmeet.file.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
--- file-service/src/main/java/com/onmeet/file/exception/GlobalExceptionHandler.java
+++ file-service/src/main/java/com/onmeet/file/exception/GlobalExceptionHandler.java
@@ -1,15 +1,17 @@
-package com.onmeet.image.exception;
+package com.onmeet.file.exception;
 
 import com.onmeet.common.exception.ErrorResponse;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
