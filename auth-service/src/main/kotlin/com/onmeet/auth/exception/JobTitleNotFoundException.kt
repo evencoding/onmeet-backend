f7b0f2ad6b381d9d8a3b32d1a6f5aa87977516b4
sprtms16 <sprtms16@gmail.com>
2026-02-08 12:22:38 +0900

    refactor(auth): implement granular custom exception hierarchy and update global handler

---
+++ auth-service/src/main/kotlin/com/onmeet/auth/exception/JobTitleNotFoundException.kt
@@ -0,0 +1,5 @@
+package com.onmeet.auth.exception
+
+import com.onmeet.common.exception.EntityNotFoundException
+
+class JobTitleNotFoundException(message: String) : EntityNotFoundException(message)
