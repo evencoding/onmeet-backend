f652c5c265bb82ffb2bb04f7f13ec34da1da80cd
sprtms16 <sprtms16@gmail.com>
2026-02-08 14:17:35 +0900

    feat(common): Add InsufficientPermissionException

---
+++ common-security/src/main/java/com/onmeet/common/exception/InsufficientPermissionException.java
@@ -0,0 +1,7 @@
+package com.onmeet.common.exception;
+
+public class InsufficientPermissionException extends RuntimeException {
+    public InsufficientPermissionException(String message) {
+        super(message);
+    }
+}
