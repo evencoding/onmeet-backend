9b563155afbda90840a1454b20163db649b34a5a
sprtms16 <sprtms16@gmail.com>
2026-02-09 13:58:20 +0900

    chore: remove legacy Kotlin files from file-service

--- file-service/src/main/kotlin/com/onmeet/file/dto/AuthPermissionResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.onmeet.file.dto
-
-data class AuthPermissionResponse(
-    val userId: Long,
-    val roles: Set<String>,
-    val companyId: Long?,
-    val teamIds: List<Long>
-)
--- file-service/src/main/resources/db/migration/V2__add_category_to_file_metadata.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE file_metadata ADD COLUMN category VARCHAR(50) NOT NULL AFTER file_name;
