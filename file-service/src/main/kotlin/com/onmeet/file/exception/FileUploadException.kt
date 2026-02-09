064cb5ec17d2238abcd6c9018ccb4d02562daaea
sprtms16 <sprtms16@gmail.com>
2026-02-09 13:58:20 +0900

    chore: remove legacy Kotlin files from file-service

--- file-service/src/main/kotlin/com/onmeet/file/exception/FileUploadException.kt
+++
@@ -1,6 +0,0 @@
-package com.onmeet.file.exception
-
-class FileUploadException : RuntimeException {
-    constructor(message: String) : super(message)
-    constructor(message: String, cause: Throwable) : super(message, cause)
-}
