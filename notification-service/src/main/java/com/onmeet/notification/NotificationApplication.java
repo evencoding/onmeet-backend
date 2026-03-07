bc3283e28c1eb419341e6fb62f7b3bed1b1a0fd6
yesung1215 <xocds991215@gmail.com>
2026-03-07 15:53:51 +0900

    feat: FCM 푸시 재시도(Spring Retry) 및 알림 자동 정리 스케줄러 추가

--- notification-service/src/main/java/com/onmeet/notification/NotificationApplication.java
+++ notification-service/src/main/java/com/onmeet/notification/NotificationApplication.java
@@ -2,10 +2,12 @@ package com.onmeet.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @org.springframework.data.jpa.repository.config.EnableJpaAuditing
 @org.springframework.scheduling.annotation.EnableScheduling
+@EnableRetry
 public class NotificationApplication {
 
     public static void main(String[] args) {
