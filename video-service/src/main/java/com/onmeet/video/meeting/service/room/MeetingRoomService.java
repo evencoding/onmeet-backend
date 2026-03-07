34249c9414a797ec4c860c064536868587846352
yesung1215 <xocds991215@gmail.com>
2026-03-07 17:35:32 +0900

    chore: video-service 내 알림 연동 TODO 주석 컨벤션 통일 ([Notification Service])

--- video-service/src/main/java/com/onmeet/video/meeting/service/room/MeetingRoomService.java
+++ video-service/src/main/java/com/onmeet/video/meeting/service/room/MeetingRoomService.java
@@ -251,7 +251,7 @@ public class MeetingRoomService {
                 participantName,
                 grants);
 
-        // TODO: [video-service] Kafka 알림 이벤트 추가 - PARTICIPANT_JOINED_NOTIFY
+        // TODO: [Notification Service] Kafka 알림 이벤트 추가 - PARTICIPANT_JOINED_NOTIFY
         eventPublisher.publishParticipantJoined(
                 new ParticipantEvent("PARTICIPANT_JOINED", roomId, userId, now));
 
@@ -288,7 +288,7 @@ public class MeetingRoomService {
         room.start(now);
 
         int participantCount = participantRepository.countActiveParticipants(roomId);
-        // TODO: [video-service] Kafka 알림 이벤트 추가 - MEETING_STARTED
+        // TODO: [Notification Service] Kafka 알림 이벤트 추가 - MEETING_STARTED
         eventPublisher.publishMeetingStarted(
                 new MeetingEvent("MEETING_STARTED", roomId, userId, participantCount, now, null));
 
@@ -461,7 +461,7 @@ public class MeetingRoomService {
         MeetingRoom saved = roomRepository.save(room);
         settingsRepository.save(RoomSettings.createDefault(saved));
 
-        // TODO: [video-service] Kafka 알림 이벤트 추가 - SCHEDULE_CREATED
+        // TODO: [Notification Service] Kafka 알림 이벤트 추가 - SCHEDULE_CREATED
 
         return toResponse(saved);
     }
@@ -492,7 +492,7 @@ public class MeetingRoomService {
         validateNoScheduleConflict(room.getHostUserId(), scheduledAt, roomId);
 
         room.updateSchedule(scheduledAt);
-        // TODO: [video-service] Kafka 알림 이벤트 추가 - SCHEDULE_CHANGED
+        // TODO: [Notification Service] Kafka 알림 이벤트 추가 - SCHEDULE_CHANGED
         return toResponse(room);
     }
 
@@ -506,7 +506,7 @@ public class MeetingRoomService {
         }
 
         room.cancel();
-        // TODO: [video-service] Kafka 알림 이벤트 추가 - SCHEDULE_CANCELLED
+        // TODO: [Notification Service] Kafka 알림 이벤트 추가 - SCHEDULE_CANCELLED
     }
 
     @Transactional(readOnly = true)
