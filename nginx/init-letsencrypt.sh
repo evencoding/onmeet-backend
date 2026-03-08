9558e91c6c6ed146ee6aedf2628933f8e3cff987
sprtms16 <sprtms16@gmail.com>
2026-03-08 10:16:07 +0900

    fix(nginx): update certbot to include rtc.onmeet.cloud SSL domain

--- nginx/init-letsencrypt.sh
+++ nginx/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! docker compose version > /dev/null 2>&1; then
   exit 1
 fi
 
-domains=(api.onmeet.cloud)
+domains=(api.onmeet.cloud rtc.onmeet.cloud)
 rsa_key_size=4096
 data_path="./nginx/certbot"
 email="evenhancoding@gmail.com"
